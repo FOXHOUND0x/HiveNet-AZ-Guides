9e75ae1f54eb2c03fb9178e79a292c7828f8f215
The 0x <0xvmx@proton.me>
2024-08-10 15:35:22 -0500

    base

---
+++ az-104/modules/Basics/Resource-Group/vars.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  description = "The location of the storage account"
+  default = "eastus"
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  default = "the0xsa"
+}
+
+variable "tags" {}
